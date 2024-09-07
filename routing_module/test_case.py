5f15b1a97bc9e515c37042312db68691edb2536c
Михаил Марин <mmn-83@yandex.ru>
2024-09-07 17:10:03 +0300

    SLC

--- routing_module/test_case.py
+++ routing_module/test_case.py
@@ -30,8 +30,8 @@ map1.add_link(Link(v2, v4))
 map1.add_link(Link(v3, v4))
 map1.add_link(Link(v4, v5))
 
-assert len(map1._links) == 5, "неверное число связей в списке _links класса LinkedGraph"
-assert len(map1._nodes) == 5, "неверное число вершин в списке _vertex класса LinkedGraph"
+assert len(map1._links) == 5, "неверное число связей в списке _links класса Graph"
+assert len(map1._nodes) == 5, "неверное число вершин в списке _nodes класса Graph"
 
 map1.add_link(Link(v2, v1))
 assert len(map1._links) == 5, "метод add_link() добавил связь Link(v2, v1), хотя уже имеется связь Link(v1, v2)"
@@ -41,7 +41,7 @@ print(path)
 
 s = path[1]
 print(s)
-assert s == 3, "неверная суммарная длина маршрута, возможно, некорректно работает объект-свойство dist"
+assert s == 3, "неверная суммарная длина маршрута, возможно, некорректно работает объект-свойство cost"
 
 assert issubclass(Station, Node), "класс Station должен наследоваться от класса Vertex"
 
@@ -60,7 +60,7 @@ map2.add_link(Link(v3, v4, 3))
 map2.add_link(Link(v4, v5, 1))
 
 assert len(map2._links) == 5, "неверное число связей в списке _links класса LinkedGraph"
-assert len(map2._nodes) == 5, "неверное число вершин в списке _vertex класса LinkedGraph"
+assert len(map2._nodes) == 5, "неверное число вершин в списке _nodes класса LinkedGraph"
 
 path = map2.find_path(v1, v5)
 print(path)
@@ -92,5 +92,3 @@ print(len(map_metro._nodes))
 path = map_metro.find_path(v1, v6)  # от сретенского бульвара до китай-город 1
 print(path[0], path[1])    # [Сретенский бульвар, Тургеневская, Китай-город 2, Китай-город 1]
 # print(sum([x.dist for x in path[1]]))  # 7
-
-
